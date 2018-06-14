73bcacfd2a41559127bbdc482950e936fd3bad85
Josh <jtrolltide@gmail.com>
2018-06-13 20:30:42 -0500

    big commit

--- _plugins/video_embed.rb
+++ _plugins/video_embed.rb
@@ -1,43 +1,46 @@
-class YouTube < Liquid::Tag
-  def initialize(tagName, markup, tokens)
-    super
+module Jekyll
+  class VideoEmbed < Liquid::Tag
 
-    args = markup.split(' ')
-    @id = args[0]
-    @width = 640
-    @height = 360
+    Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
-    if args[1]
-      @width = args[1]
-      @height = args[2]
+    Hosts = {
+      "ted"     => ->(id) { "https://embed-ssl.ted.com/talks/#{id}.html" },
+      "ustream" => ->(id) { "https://ustream.tv/embed/#{id}" },
+      "vimeo"   => ->(id) { "https://player.vimeo.com/video/#{id}" },
+      "youtube" => ->(id) { "https://youtube.com/embed/#{id}" },
+      "twitch"  => ->(id) { "https://player.twitch.tv/?autoplay=false&muted=false&channel=#{id}" },
+      "twitchclip"  => ->(id) { "https://clips.twitch.tv/embed?autoplay=false&clip=#{id}" }
+    }
+
+    def initialize(tag_name, markup, tokens)
+      super
+
+      if markup =~ Syntax then
+        @host = Hosts[tag_name]
+        @id = $1
+
+        if $2.nil? then
+            @width = 640
+            @height = 360
+        else
+            @width = $2.to_i
+            @height = $3.to_i
+        end
+      else
+        raise "No video ID provided in the \"#{tag_name}\" tag"
+      end
     end
-  end
 
-  def render(context)
-    "<div class=\"responsive-video-container\"><iframe width=\"#{@width}\" height=\"#{@height}\" src=\"https://www.youtube-nocookie.com/embed/#{@id}?showinfo=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen class=\"lazyload\"></iframe></div>"
-  end
-
-  Liquid::Template.register_tag("youtube", self)
-end
-
-class Vimeo < Liquid::Tag
-  def initialize(tagName, markup, tokens)
-    super
-
-    args = markup.split(' ')
-    @id = args[0]
-    @width = 640
-    @height = 360
-
-    if args[1]
-      @width = args[1]
-      @height = args[2]
+    def render(context)
+      %Q[
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@host.call(@id)}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+  </iframe>
+</div>
+        ]
     end
-  end
+    
+    Hosts.each_key { |key| Liquid::Template.register_tag key, self }
 
-  def render(context)
-    "<div class=\"responsive-video-container\"><iframe width=\"#{@width}\" height=\"#{@height}\" src=\"https://player.vimeo.com/video/#{@id}?title=0&amp;byline=0\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen class=\"lazyload\"></iframe></div>"
   end
-
-  Liquid::Template.register_tag("vimeo", self)
 end
--- index.md
+++ index.md
@@ -3,14 +3,14 @@
 # Edit theme's home layout instead if you wanna make some changes
 # See: https://jekyllrb.com/docs/themes/#overriding-theme-defaults
 layout: defaulthome
-title: HOMEPAGE
+title: Home
 ---
 
 Josh Thomas is a Junior System Administrator currently located in Birmingham, AL.
 
 He works mainly within the Microsoft Windows environment, though he has been known to dabble in the GNU/FOSS world of Ubuntu and CentOS & in the future of containers with Docker.
 
-During his free time he enjoys cooking for his wife and playing with his 10-yr-old beagle Buster. He is currently obsessed with making the perfect homemade pizza dough.
+During his free time he enjoys cooking for his wife and playing with his 10-yr-old beagle [Buster](https://www.instagram.com/p/XTidm9jaMU/). He is currently obsessed with making the perfect homemade pizza dough.
 
 If you are interested in hiring him, you can view his up-to-date [resume](https://drive.google.com/file/d/1V2KL5vjbBWJ5V3BiM3qheILJduaHHYMl/view?usp=sharing). 
 
