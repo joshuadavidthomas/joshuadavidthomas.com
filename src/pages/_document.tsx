c95e1188e9051aea3a3f6af4aa05573bd196ae83
Josh <josh@joshthomas.dev>
2022-01-17 17:00:50 -0600

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,6 +25,7 @@ export default class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
+          <script defer data-domain="joshthomas.dev" src="https://stats.joshthomas.dev/js/plausible.js"></script>
         </Head>
         <body className="text-gray-800 dark:text-gray-100 bg-gray-50 dark:bg-gray-800">
           <Container>
