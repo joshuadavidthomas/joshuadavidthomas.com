59559b8525a1a1e22fff3ae5784a520bc7a8ecd4
Automated Publisher <actions@users.noreply.github.com>
2023-11-14 18:32:26 +0000

    :bookmark: bump version 2023.11.15 -> 2023.11.16

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-__version__ = "2023.11.15"
+__version__ = "2023.11.16"
--- tests/test_version.py
+++ tests/test_version.py
@@ -4,4 +4,4 @@ from config import __version__
 
 
 def test_version():
-    assert __version__ == "2023.11.15"
+    assert __version__ == "2023.11.16"
