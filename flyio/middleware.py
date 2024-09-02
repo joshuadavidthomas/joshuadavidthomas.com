8cf3850c1e4252ea4c8d17b7bcb6e438bfe62324
Josh <josh@joshthomas.dev>
2024-09-02 11:06:58 -0500

    try to catch sqlite and django readonly exceptions as well

--- flyio/middleware.py
+++ flyio/middleware.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import sqlite3
+
+from django.db.utils import OperationalError
 from django.http import HttpResponse
 
 from .exceptions import WritesAttemptedError
@@ -12,7 +15,12 @@ def replay_middleware(get_response):
     def middleware(request):
         try:
             response = get_response(request)
-        except WritesAttemptedError:
+        except (WritesAttemptedError, sqlite3.OperationalError, OperationalError) as e:
+            if isinstance(e, (sqlite3.OperationalError, OperationalError)):
+                if "readonly database" not in str(e).lower():
+                    # Re-raise the exception if it's not a readonly database error
+                    raise
+
             response = HttpResponse()
             primary = get_primary_instance()
             response[FLY_REPLAY] = f"instance={primary}"
